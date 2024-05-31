0f132f958617a837774a6356ae462524ee3a8623
zeroking0429 <k36379694@gmail.com>
2024-05-31 20:11:58 +0900

    v0.8

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,9 +1,39 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: auto;
+  height: auto;
+`
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  border: 7px solid;
+  border-color: white gray gray white;
+  font-size: 3em;
+  margin: 20px 0;
+  padding: 7px 14px;
+  margin-right: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    border-color: gray white white gray;
+  }
+`
+
+const Text = styled.p`
+  font-size: 2.5em;
+  margin-bottom: 10px;
+`
 
 export default function Main() {
-  const [lang, setLang] = useState("kr");
-
   return (
-    <h1>Main!</h1>
+    <Wrapper>
+      <Button>Click</Button>
+      <Button>Sell</Button>
+      <Text>Items : 0</Text>
+      <Text>Money : 0</Text>
+    </Wrapper>
   );
 }
