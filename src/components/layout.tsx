69808382e02f78a25fa03a0dee8b29e5586e0168
zeroking0429 <k36379694@gmail.com>
2024-05-31 19:54:05 +0900

    v0.7

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,32 @@
 import { Link, Outlet } from "react-router-dom";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const MenuWrapper = styled.div`
   display: block;
   justify-content: flex-start;
   align-items: flex-start;
   padding: 20px;
   border: 2px solid white;
+  a {
+    font-size: 3em;
+    text-decoration: none;
+    color: white;
+    margin-right: 30px;
+  }
+`
+
+const Wrapper = styled.div`
+  margin: 20px;
 `
 
 export default function Layout() {
   return (
-    <>
-      <Wrapper>
+    <Wrapper>
+      <MenuWrapper>
         <Link to={"/main"}>Main</Link>
         <Link to={"/main/upgrade"}>Upgrade</Link>
-      </Wrapper>
+      </MenuWrapper>
       <Outlet />
-    </>
+    </Wrapper>
   );
 }
